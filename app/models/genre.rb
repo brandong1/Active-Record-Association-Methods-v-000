d164253a2e2805203aee227441abaea84888769d
Brandon Green <brandon@brandonleeproductions.com>
2018-08-28 00:22:56 +0000

    automatically backed up by learn

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -15,7 +15,6 @@ class Genre < ActiveRecord::Base
   def all_artist_names
     # return an array of strings containing every musician's name
     self.artists.collect do |n|
-      n = artists.name
       n.name
     end
     #binding.pry
